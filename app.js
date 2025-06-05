a4170f2674ab23c8fb1776d645e3c6f46143f982
Kiyoshi Takahama <k1y05h17k@gmail.com>
2025-06-05 18:38:21 -0300

    feat(app): add patch request

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.get('/api/v1/tours', (req, res) => {
         status: 'success',
         results: tours.length,
         data: {
-          tours
+            tours
         }
     });
 });
@@ -35,7 +35,7 @@ app.get('/api/v1/tours/:id', (req, res) => {
     const id = req.params.id * 1;
     const tour = tours.find(el => el.id === id);
 
-    if(!tour){
+    if (!tour) {
         return res.status(404).json({
             status: 'fail',
             message: 'Invalid ID'
@@ -55,9 +55,9 @@ app.post('/api/v1/tours', (req, res) => {
 
     const newId = tours[tours.length - 1].id + 1;
     const newTour = Object.assign({ id: newId }, req.body);
-    
+
     tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours), err => {
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
         res.status(201).json({
             status: 'success',
             data: {
@@ -67,7 +67,27 @@ app.post('/api/v1/tours', (req, res) => {
     });
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
 
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
+
+    if (!tour) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: '<Updated tour here...>'
+        }
+    })
+
+
+});
 
 
 
