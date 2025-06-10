db06b8abea2af6e9a12098323784f641e676ca93
Kiyoshi Takahama <k1y05h17k@gmail.com>
2025-06-10 17:21:06 -0300

    feat(routes/userRoutes) refactor code

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,49 +1,15 @@
 const express = require('express');
-
-const getAllUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route not yet defined!'
-    });
-};
-
-const createUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route not yet defined!'
-    });
-};
-
-const getUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route not yet defined!'
-    });
-};
-
-const updateUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route not yet defined!'
-    });
-};
-
-const deleteUser = (req, res) => {
-    res.status(500).json({
-        status: error,
-        message: 'This route not yet defined!'
-    });
-};
+const userController = require('./../controllers/userController');
 
 const router = express.Router();
 
 router.route('/')
-.get(getAllUser)
-.post(createUser);
+.get(userController.getAllUser)
+.post(userController.createUser);
 
 router.route('/:id')
-.get(getUser)
-.patch(updateUser)
-.delete(deleteUser)
+.get(userController.getUser)
+.patch(userController.updateUser)
+.delete(userController.deleteUser)
 
 module.exports = router;
