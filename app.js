4c413ee76dd278623323125e784cccbd177941f6
Kiyoshi Takahama <k1y05h17k@gmail.com>
2025-06-10 16:49:46 -0300

    feat(app.js) creating and mounting multiples routes

--- app.js
+++ app.js
@@ -151,25 +151,29 @@ const deleteUser = (req, res) => {
 
 // 3) ROUTES
 
-app.route('/api/v1/tours')
+const tourRouter = express.Router();
+const userRouter = express.Router();
+
+tourRouter.route('/')
 .get(getAllTour)
 .post(createTour);
 
-app.route('/api/v1/tours/:id')
+tourRouter.route('/:id')
 .get(getTour)
 .patch(updateTour)
 .delete(deleteTour);
 
-app.route('/api/v1/users')
+userRouter.route('/')
 .get(getAllUser)
 .post(createUser);
 
-app.route('/api/v1/users/:id')
+userRouter.route('/:id')
 .get(getUser)
 .patch(updateUser)
 .delete(deleteUser)
 
-
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 // 4) START SERVER
 
 const port = 3000;
