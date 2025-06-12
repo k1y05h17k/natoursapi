b94ede6ad05d3ab61fb048c7d94babab5607e49d
Kiyoshi Takahama <k1y05h17k@gmail.com>
2025-06-12 15:24:28 -0300

    feat(config.env) create file to all environment variables

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
+
 const app = express();
 
 // 1) MIDDLEWARES
+console.log(process.env.NODE_ENV);
+if(process.env.NODE_ENV === 'development'){
 app.use(morgan('dev'));
+}
 
 app.use(express.json());
 
