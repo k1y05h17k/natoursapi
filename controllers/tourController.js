dd319a753573b64b8e6c93b2969ac57468b6a239
Kiyoshi Takahama <k1y05h17k@gmail.com>
2025-06-26 17:25:49 -0300

    feat(tourController) add new way of deleting documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -9,7 +9,7 @@ exports.getAllTour = async (req, res) => {
       requestedAt: req.requestTime,
       results: tours.length,
       data: {
-        tours
+        tours,
       },
     });
   } catch (err) {
@@ -22,15 +22,15 @@ exports.getAllTour = async (req, res) => {
 
 exports.getTour = async (req, res) => {
   try {
-    const tour = await Tour.findById(req.params.id)
+    const tour = await Tour.findById(req.params.id);
 
     res.status(200).json({
-    status: 'success',
-    data: {
-      tour
-    },
-  });
-  } catch (er){
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (er) {
     res.status(400).json({
       status: 'fail',
       message: err,
@@ -81,9 +81,18 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
