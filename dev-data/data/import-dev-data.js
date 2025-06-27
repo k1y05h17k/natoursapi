17205db6936985983f0372faed555d9f4275c201
Kiyoshi Takahama <k1y05h17k@gmail.com>
2025-06-27 18:17:15 -0300

    feat(import-dev-data) create script to import data to collection

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -5,56 +5,116 @@ const Tour = require('./../../models/tourModel');
 // const Review = require('./../../models/reviewModel');
 // const User = require('./../../models/userModel');
 
-dotenv.config({ path: './config.env' });
+  dotenv.config({ path: './config.env' });
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
+  const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+  );
 
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  })
-  .then(() => console.log('DB connection successful!'));
+  mongoose
+    .connect(DB, {})
+    .then(() => console.log('DB connection successful!'));
 
-// READ JSON FILE
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
-// const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
-// const reviews = JSON.parse(
-//   fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
-// );
+  // READ JSON FILE
+  const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
 
-// IMPORT DATA INTO DB
-const importData = async () => {
-  try {
-    await Tour.create(tours);
-    // await User.create(users, { validateBeforeSave: false });
-    // await Review.create(reviews);
-    console.log('Data successfully loaded!');
-  } catch (err) {
-    console.log(err);
+  // IMPORT DATA INTO DB
+  const importData = async () => {
+    try {
+      await Tour.create(tours);
+      console.log('Data successfully loaded!');
+      process.exit();
+    }catch (err){
+      console.log(err);
+    }
+  };
+
+  // DELETE ALL DATA FROM COLLECTION
+  const deleteData = async () => {
+    try {
+      await Tour.deleteMany();
+      console.log('Data successfully deleted!');
+      process.exit();
+    }catch (err) {
+      console.log(err);
+    }
+  } 
+
+  if(process.argv[2] == '--import'){
+    importData();
+  }else if(process.argv[2] == '--delete'){
+    deleteData();
   }
-  process.exit();
-};
+  console.log(process.argv);
 
-// DELETE ALL DATA FROM DB
-const deleteData = async () => {
-  try {
-    await Tour.deleteMany();
-    // await User.deleteMany();
-    // await Review.deleteMany();
-    console.log('Data successfully deleted!');
-  } catch (err) {
-    console.log(err);
-  }
-  process.exit();
-};
 
-if (process.argv[2] === '--import') {
-  importData();
-} else if (process.argv[2] === '--delete') {
-  deleteData();
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+// // READ JSON FILE
+// const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+// // const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+// // const reviews = JSON.parse(
+// //   fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+// // );
+
+// // IMPORT DATA INTO DB
+// const importData = async () => {
+//   try {
+//     await Tour.create(tours);
+//     // await User.create(users, { validateBeforeSave: false });
+//     // await Review.create(reviews);
+//     console.log('Data successfully loaded!');
+//   } catch (err) {
+//     console.log(err);
+//   }
+//   process.exit();
+// };
+
+// // DELETE ALL DATA FROM DB
+// const deleteData = async () => {
+//   try {
+//     await Tour.deleteMany();
+//     // await User.deleteMany();
+//     // await Review.deleteMany();
+//     console.log('Data successfully deleted!');
+//   } catch (err) {
+//     console.log(err);
+//   }
+//   process.exit();
+// };
+
+// if (process.argv[2] === '--import') {
+//   importData();
+// } else if (process.argv[2] === '--delete') {
+//   deleteData();
+// }
