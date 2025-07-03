9953d771853fe996107b982ea7b9992051417723
Kiyoshi Takahama <k1y05h17k@gmail.com>
2025-07-03 16:23:12 -0300

    feat(tourController) add advanced filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,12 +3,17 @@ const Tour = require('./../models/tourModel');
 exports.getAllTour = async (req, res) => {
   try {
     // Build Query
+    //  1) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach(el => delete queryObj[el]);
-    const query = Tour.find(queryObj);
+    
+    // 2) Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    console.log(JSON.parse(queryStr));
 
-    console.log(req.query, queryObj);
+    const query = Tour.find(JSON.parse(queryStr));
 
     // Execute Query
     const tours = await query;
