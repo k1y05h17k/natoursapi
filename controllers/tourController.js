471808bdf273ac712a0ec946ff973ffe192eedee
Kiyoshi Takahama <k1y05h17k@gmail.com>
2025-07-07 16:58:30 -0300

    feat(tourController) Making API Better: Limit Fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,27 +3,34 @@ const Tour = require('./../models/tourModel');
 exports.getAllTour = async (req, res) => {
   try {
     // Build Query
-    //  1A) Filtering
+
+    // 1A) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach(el => delete queryObj[el]);
-    
+
     // 1B) Advanced Filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
 
-     let query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
 
-    // 2)Sorting
-    if(req.query.sort) {
+    // 2) Sorting
+    if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
       console.log(sortBy);
       query = query.sort(sortBy);
-    }else {
+    } else {
       query = query.sort('-createAt');
     }
 
-   
+    // 3) Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v')
+    }
 
     // Execute Query
     const tours = await query;
