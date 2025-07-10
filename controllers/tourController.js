c325efaaba9d10c9504a5989fe9bfcaf35af5358
Kiyoshi Takahama <k1y05h17k@gmail.com>
2025-07-10 15:04:34 -0300

    feat(utils/apiFeature.js) Refactoring API Features

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,62 +1,11 @@
 const Tour = require('./../models/tourModel');
-
+const APIFeatures = require('./../utils/apiFeatures');
 exports.aliasTopTour = async (req, res, next) => {
   req.query.limit = '5';
   req.query.sort = '-ratingsAverage,price';
   req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
   next();
 };
-
-class APIFeatures {
-  constructor(query, queryString) {
-    this.query = query;
-    this.queryString = queryString;
-  }
-
-  filter() {
-    const queryObj = { ...this.queryString };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach(el => delete queryObj[el]);
-  
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-
-    this.query.find(JSON.parse(queryStr));
-
-    return this;
-  }
-  sort(){
-      if (this.queryString.sort) {
-      const sortBy = this.queryString.sort.split(',').join(' ');
-      console.log(sortBy);
-      this.query = this.query.sort(sortBy);
-    } else {
-      this.query = this.query.sort('-createAt');
-    }
-    return this;
-
-  }
-  limitFields(){
-     if (this.queryString.fields) {
-      const fields = this.queryString.fields.split(',').join(' ');
-      this.query = this.query.select(fields);
-    } else {
-      this.query = this.query.select('-__v')
-    }
-    return this;
-  }
-
-  paginate(){
-    const page = this.queryString.page * 1 || 1; // The way to converted to number
-    const limit = this.queryString.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-    this.query = this.query.skip(skip).limit(limit);
-
-    return this;
-  }
-
-
-}
 
 exports.getAllTour = async (req, res) => {
   try {
